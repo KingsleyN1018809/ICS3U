4f7558b29af739ad70008ec8f2418cc2c7479046
KingsleyN1018809 <1018809@pdsb.net>
2024-09-19 10:27:36 -0400

    Create Lesson 5- Input and Output: Working With Math Formulas.py

---
+++ Lesson 5- Input and Output: Working With Math Formulas.py
@@ -0,0 +1,13 @@
+#Predict:
+import math
+length = input("Please input a length: ")
+length = float(length)
+area = math.pow(length, 2)
+print("The area of a square of side length", length, "is:", area)
+#This ask for the length then it will calculate the area of he square by using your length square
+#Modify:
+import math
+radius = input("Please input a radius: ")
+radius = float(radius)
+tarea = (0.5*math.pi*(radius**2)) + (4*(radius**2))
+print("Area of a Norman Window shape is:", tarea)
