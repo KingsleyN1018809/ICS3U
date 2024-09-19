8d25cfacf5170a4e86916019742ed89fdb40a7a9
KingsleyN1018809 <1018809@pdsb.net>
2024-09-19 10:16:19 -0400

    Create Lesson 1 - Basic Output.py

---
+++ Lesson 1 - Basic Output.py
@@ -0,0 +1,10 @@
+name = "Leslie"
+print("Hello", name)
+# Prediction: Hello Leslie
+
+MODIFY
+name = "Kingsley"
+print("Hello,", name)
+# This will print out (Hello, Kingsley). The variable name is input a string that will be declare as name, upon using the variable will use the input that you put in the variable name
+@KingsleyN1018809
+Comment
