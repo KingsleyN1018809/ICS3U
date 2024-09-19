b24eb25b9539a59bea8a32e4a3a2ddc3459c6c00
KingsleyN1018809 <1018809@pdsb.net>
2024-09-19 10:17:34 -0400

    Create Lesson 3: Display text and number values together.py

---
+++ Lesson 3: Display text and number values together.py
@@ -0,0 +1,23 @@
+#Predict and Run
+
+print("Welcome to Sanjay's Koala Party!")
+print("It's possible Sanjay can distribute ", 12)
+print(" cookies for each bowl.")
+
+#The output is:
+Welcome to Sanjay's Koala Party!
+It's possible Sanjay can distribute  12
+ cookies for each bowl.
+ 
+#Modify:
+ 
+print("Welcome to Sanjay's Koala Party!")
+print("It's possible Sanjay can distribute",12, "cookies for each bowl")
+
+#Remix and modify:
+print("Sanjay has",6 ,"bowls for", 144, "cookies")
+
+#Investigate and Modify:
+
+cookies = 6*4
+print("The number of cookies per bowl is:",cookies)
