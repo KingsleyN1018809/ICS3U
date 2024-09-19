c86ba00a391582857d1af90e59c677b8eb501320
KingsleyN1018809 <1018809@pdsb.net>
2024-09-19 10:17:58 -0400

    Create Lesson 4 - Input and Output: Dividing two numbers.py

---
+++ Lesson 4 - Input and Output: Dividing two numbers.py
@@ -0,0 +1,14 @@
+#Predict:
+num = input("Please input a whole number: ") 
+num = int(num) 
+num2 = num / 6
+print("When", num, " is divided by 6", "the result is:", num2)
+#This will ask you to input a number, then it will print out your number divide by 6
+
+#Modify:
+num = input("Please input a whole number: ") 
+num = int(num) 
+num2 = input("Please input a second whole number: ")
+num2 = int(num2)
+answer = num/num2
+print("When", num, " is divided by",num2, "the result is:", answer)
