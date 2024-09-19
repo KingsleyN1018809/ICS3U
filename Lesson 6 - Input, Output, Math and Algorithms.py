18acbcba0f5454fa25ece9583f13eb9dfaba0fbf
KingsleyN1018809 <1018809@pdsb.net>
2024-09-19 10:32:02 -0400

    Create Lesson 6 - Input, Output, Math and Algorithms.py

---
+++ Lesson 6 - Input, Output, Math and Algorithms.py
@@ -0,0 +1,7 @@
+#Code
+year = int(input("Please input the year you were born:"))
+age = int(input("Please input your age:"))
+result = (year*2) + 5
+result = (result*50)+ age
+result = (result - 250)/100
+print(result)
