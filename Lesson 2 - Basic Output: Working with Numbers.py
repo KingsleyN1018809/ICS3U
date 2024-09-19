21596739cd35058aa08b4740f68bdcb4c6083cfa
KingsleyN1018809 <1018809@pdsb.net>
2024-09-19 10:17:11 -0400

    Create Lesson 2 - Basic Output: Working with Numbers.py

---
+++ Lesson 2 - Basic Output: Working with Numbers.py
@@ -0,0 +1,38 @@
+#Predict:
+print(24)
+#This code will print out the number 24
+
+#Modify
+
+amount = 24
+print(amount)
+
+#This also give 24
+
+print("Two dozen is: ", amount)
+
+#This print: Two dozen is: 24
+
+#Modify 2
+amount = 7*4
+print("forty-two is: ", amount)
+
+#This will print out: forty-two is: 28
+
+#Modify 3
+
+amount = 8
+amount = amount + 1
+print("The amount is: ", amount)
+
+#This will print out: The amount is: 9
+#because this take the variable amount as 8 and then add in 1
+
+#Modify 4
+
+amount = 8
+amount = amount * 2
+print(amount)
+
+#This will print out: 16
+#Because this take the variable amount then multiply by 2
