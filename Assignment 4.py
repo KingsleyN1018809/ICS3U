36a5f9cfaa19719e2ca18347571e9abef0f4e349
KingsleyN1018809 <1018809@pdsb.net>
2024-12-11 10:43:12 -0500

    Create Assignment 4.py

---
+++ Assignment 4.py
@@ -0,0 +1,10 @@
+def sort(words, dates):
+    pass
+
+def merge(year, month, day):
+    months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",]
+    line_int = ""
+    line_int += year
+
+filename = 'wordle.dat'
+fh = open(filename, "r")
